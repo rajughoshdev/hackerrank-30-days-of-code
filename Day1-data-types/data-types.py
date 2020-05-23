24c0779cb9540fd4621a8ae7cb115ff2a41957ea
rajughoshdev <rajughoshdev@gmail.com>
2020-05-23 13:22:33 +0600

    added hello workd, data-types

---
+++ Day1-data-types/data-types.py
@@ -0,0 +1,11 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+userInputI = int(input())   # read int
+userInputd = float(input()) # read double 
+userInputs = str(input())   # read string
+
+print(i+userInputI)
+print(d+userInputd)
+print(s+userInputs)
